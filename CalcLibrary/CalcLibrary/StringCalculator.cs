2b83c2d02e2c8540ff3d8113416524ab351fffad
Abby Thompson <abby.thompson@c02tq0bphtd8.myob.myobcorp.net>
2019-07-30 14:15:33 +1200

    step 1 - step 4 with testing

--- CalcLibrary/CalcLibrary/StringCalculator.cs
+++ CalcLibrary/CalcLibrary/StringCalculator.cs
@@ -1,10 +1,31 @@
+using System;
+using System.Globalization;
+using System.Reflection.Metadata.Ecma335;
+
 namespace CalcLibrary
 {
     public class StringCalculator
     {
-        public int Add(string empty)
+        public int Add(string str)
         {
-            return 0;
+            switch (str.Length)
+            {
+                case 0: return 0;
+                case 1: if (int.TryParse(str, out int output)) return output; break;
+            }
+
+            var numbers = str.Split(",");
+
+            
+            var sum = 0;
+            
+            foreach (var number in numbers)
+            {
+                Console.WriteLine(int.Parse(number));
+                sum += int.Parse(number);;
+            }
+
+            return sum;
         }
     }
 }
--- CalcLibrary/TestingCalc/UnitTest1.cs
+++ CalcLibrary/TestingCalc/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using CalcLibrary;
 using Xunit;
 
 namespace TestingCalc
@@ -12,6 +13,39 @@ namespace TestingCalc
             var response = calc.Add("");
 
             Assert.Equal(0, response);
+        }
+
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("3", 3)]
+        public void Single_number_string_input_returns_that_integer(string input, int expected)
+        {
+            StringCalculator calc = new StringCalculator();
+            var response = calc.Add(input);
+
+            Assert.Equal(expected, response);
+        }
+
+        [Theory]
+        [InlineData("1,2", 3)]
+        [InlineData("3,5", 8)]
+        public void Two_numbers_in_string_input_returns_the_sum(string input, int expected)
+        {
+            StringCalculator calc = new StringCalculator();
+            var response = calc.Add(input);
+
+            Assert.Equal(expected, response);
+        }
+
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("3,5,3,9", 20)]
+        public void More_than_2_numbers_in_string_input_returns_the_sum(string input, int expected)
+        {
+            StringCalculator calc = new StringCalculator();
+            var response = calc.Add(input);
+
+            Assert.Equal(expected, response);
         }
     }
 }
